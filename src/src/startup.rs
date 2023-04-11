e85b4bb4ba7ce80d93b3d925257d769bfc9e1ba8
schoettner <michael.schoettner@hhu.de>
2023-04-11 18:00:52 +0200

    Aufgabe 2

---
+++ src/src/startup.rs
@@ -0,0 +1,29 @@
+#![feature(alloc_error_handler)] 
+
+
+extern crate alloc;  // this is for heap allocations
+
+
+
+use kernel::allocator;
+
+use user::aufgabe2::heap_demo;
+use user::aufgabe2::sound_demo;
+
+
+fn aufgabe2() {
+   heap_demo::run();
+   sound_demo::run();
+}
+
+
+#[no_mangle]
+pub extern fn startup() {
+
+    // Speicherverwaltung initialisieren
+	
+    aufgabe2();
+          
+    loop{}
+}
+

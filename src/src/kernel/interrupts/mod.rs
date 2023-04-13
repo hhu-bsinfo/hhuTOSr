f485fea98c478bc8a0de3e86f9d50cf15694b022
schoettner <michael.schoettner@hhu.de>
2023-04-13 12:10:25 +0200

    Aufgabe 3

--- src/src/kernel/interrupts/mod.rs
+++ src/src/kernel/interrupts/mod.rs
@@ -2,3 +2,19 @@
 pub mod pic;
 pub mod isr;
 pub mod intdispatcher;
+
+
+extern "C" { fn _init_interrupts(); }
+
+
+pub fn init() {
+
+    // setup IDT and PIC (in 'interrupts.asm')
+    unsafe {
+       _init_interrupts();
+    }
+    
+    // IntDispatcher initialisieren
+    intdispatcher::init();
+
+}
